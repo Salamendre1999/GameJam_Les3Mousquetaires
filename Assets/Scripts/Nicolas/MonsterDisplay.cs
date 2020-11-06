a51ca1f73d830e4263b99a4d4af47511067f03f1
Nico <nicola170799@gmail.com>
2020-11-06 23:23:55 +0100

    scriptable object

---
+++ Assets/Scripts/Nicolas/MonsterDisplay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nicolas
+{
+    public class MonsterDisplay : MonoBehaviour
+    {
+
+        [SerializeField] private Monster monster;
+
+        
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+          
+    Debug.Log(monster.monsterName);
+
+
+
+        }
+
+
+    }
+}
