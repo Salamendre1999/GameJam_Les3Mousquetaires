8e17e928537a27043a9bf0b6b531ca790538d454
Nico <nicola170799@gmail.com>
2020-11-07 00:44:46 +0100

    menu

--- Assets/Scripts/Nicolas/MonsterDisplay.cs
+++ Assets/Scripts/Nicolas/MonsterDisplay.cs
@@ -6,16 +6,23 @@ namespace Nicolas
     public class MonsterDisplay : MonoBehaviour
     {
 
-        [SerializeField] private Monster monster;
-
+        [SerializeField] private Text nameText;
+        [SerializeField] private Image icon;
+        [SerializeField] private Text damage;
+        [SerializeField] private Text health;
+        public static MonsterDisplay Instance;
+        [SerializeField] private Monster datas;
         
         
         // Start is called before the first frame update
         void Start()
         {
-          
-    Debug.Log(monster.monsterName);
 
+            Instance = this;
+            Instance.nameText.text = datas.name;
+            Instance.icon.sprite = datas.artwork;
+            Instance.damage.text = datas.damage.ToString();
+            Instance.health.text = datas.health.ToString();
 
 
         }
