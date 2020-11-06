20b79d915e9f854fcd93fc620d2f5bd687e396f8
fox <ahmedrida21@gmail.com>
2020-11-06 19:26:55 +0100

    Added player movment and map creation with all the assets

---
+++ Assets/Scripts/Ahmed/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D playerRigidBody2D;
+    private bool lookAtRight = true;
+    private void Awake()
+    {
+        playerRigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(float horizontalMove, float verticalMove, float speed)
+    {
+        Vector3 movement = new Vector2(horizontalMove,verticalMove);
+        playerRigidBody2D.velocity = movement * speed;
+
+        if (horizontalMove < 0 && lookAtRight)
+        {
+            FlipSprite();
+        } else if(horizontalMove>0 && !lookAtRight)
+        {
+            FlipSprite();
+        }
+    }
+
+    private void FlipSprite()
+    {
+        lookAtRight = !lookAtRight;
+        
+        //cant change directly a struct
+        Vector3 scaleTemp = transform.localScale;
+        scaleTemp.x *= -1;
+        transform.localScale = scaleTemp;
+    }
+}
---
+++ Assets/Scripts/Ahmed/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float horizontalMove;
+    private float verticalMove;
+    [SerializeField] private CharacterController characterController;
+
+    [SerializeField] private float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * speed;
+        verticalMove = Input.GetAxis("Vertical") * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        characterController.Move(horizontalMove*Time.deltaTime,verticalMove*Time.deltaTime, speed);
+    }
+}
