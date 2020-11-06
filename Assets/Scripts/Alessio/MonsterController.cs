4ba8b96f46250d03f4639fa419e6cc76fe6b29d4
Alessio <alessio.soldi@student.vinci.be>
2020-11-06 23:02:03 +0100

    Bat creation

---
+++ Assets/Scripts/Alessio/MonsterController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Alessio
+{
+    public class MonsterController : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private float moveSpeed;
+        private Rigidbody2D _rigidbody2D;
+        private Vector2 _movement;
+
+        private void Awake()
+        {
+            _rigidbody2D = this.GetComponent<Rigidbody2D>();
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        private void Update()
+        {
+            Vector3 direction = target.position - transform.position;
+            direction.Normalize();
+            _movement = direction;
+        }
+
+        private void FixedUpdate()
+        {
+            MoveEnnemy(_movement);
+        }
+
+        void MoveEnnemy(Vector2 direction)
+        {
+            _rigidbody2D.MovePosition((Vector2)transform.position + (direction * (moveSpeed * Time.deltaTime)));
+        }
+    }
+}
