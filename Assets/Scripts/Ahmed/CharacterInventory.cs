b8164a8bc01c649d48f9e5d684daa445b8dc7d50
fox <ahmedrida21@gmail.com>
2020-11-07 20:39:03 +0100

    added atlas to fix pixel bugs, added more lights, arranged some inputs,etc

---
+++ Assets/Scripts/Ahmed/CharacterInventory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInventory : MonoBehaviour
+{
+    public bool hasAKey;
+    public int keyNumber;
+}
---
+++ Assets/Scripts/Ahmed/DoorHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHandler : MonoBehaviour
+{
+    [SerializeField] private GUIStyle style;
+    [SerializeField] private Sprite openDoorSprite;
+    [SerializeField] private Sprite closedDoorSprite;
+    [SerializeField] private int doorNumber;
+    [SerializeField] private BoxCollider2D collider;
+    private GameObject character;
+    private bool isOpen;
+    private bool inRange;
+    private bool goodKey;
+    private bool clicked;
+
+
+    private void Update()
+    {
+        if (inRange && goodKey && Input.GetButtonDown("Attack"))
+        {
+            clicked = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (clicked && !isOpen)
+        {
+            Debug.Log("i enter here lets goo");
+            character.GetComponent<CharacterInventory>().keyNumber = 0;
+            character.GetComponent<CharacterInventory>().hasAKey = false;
+            gameObject.GetComponent<SpriteRenderer>().sprite = openDoorSprite;
+            collider.enabled = false;
+            isOpen = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.parent.gameObject.GetComponent<PlayerMovement>())
+        {
+            inRange = true;
+            character = other.transform.parent.gameObject;
+            if (character.GetComponent<CharacterInventory>().keyNumber == doorNumber)
+            {
+                goodKey = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        if (inRange && goodKey && !isOpen)
+        {
+            GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 100f, 100f), "PRESS A TO OPEN THE DOOR",style);
+
+        }
+    }
+}
