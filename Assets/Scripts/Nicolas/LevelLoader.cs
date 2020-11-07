a02a5db1a874a967a876ff000182279810eff3ff
Nico <nicola170799@gmail.com>
2020-11-07 18:11:24 +0100

    gamemanager

---
+++ Assets/Scripts/Nicolas/LevelLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Nicolas
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        public void Load(string levelName)
+        {
+            GameManager.Instance.LoadScene(levelName);
+        }
+        
+
+    }
+}
