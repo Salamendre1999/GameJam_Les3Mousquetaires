38e10c49d55bdb5af804cbe7bb752160ef0a4c1d
Nico <nicola170799@gmail.com>
2020-11-07 18:22:13 +0100

    gamemanager

--- Assets/Scripts/Nicolas/gameManager.cs
+++ Assets/Scripts/Nicolas/gameManager.cs
@@ -5,43 +5,41 @@ namespace Nicolas
 {
     public class GameManager : Singleton<GameManager>
     {
-        
-
+            
+    
+        protected override void Initialize()
+        {
+        }
+        protected override void Cleanup()
+        {
+        }
+    
         public void NewGame()
         {
             LoadScene("Level1");
-           
+                
         }
-
+    
         public void GameOver()
         {
             LoadScene("GameOver");
         }
-
+    
         public void MainMenu()
         {
             LoadScene("MainMenu");
         }
-
+    
         public void LoadScene(string sceneName)
         {
             SceneManager.LoadScene(sceneName);
             var asyncload = SceneManager.LoadSceneAsync(sceneName);
         }
-
+    
         public void ExitGame()
         {
             Application.Quit();
         }
-
-        protected override void Cleanup()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void Initialize()
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
+
